fa30676f899af4d90fbb0f4588ebc500b3b687b7
Charles Bousseau <cbousseau@anaconda.com>
2024-06-19 16:30:18 -0400

    update to b3184

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,6 @@ if [[ ${gpu_variant:0:5} = "cuda-" ]]; then
     if [[ ${gpu_variant:-} = "cuda-11" ]]; then
         export CUDACXX=/usr/local/cuda/bin/nvcc
         export CUDAHOSTCXX="${CXX}"
-    else
-        # cuda-compat provided libcuda.so.1
-        LDFLAGS="$LDFLAGS -Wl,-rpath-link,${PREFIX}/cuda-compat/"
     fi
 elif [[ ${gpu_variant:-} = "none" ]]; then
     LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_CUDA=OFF"
@@ -30,8 +27,9 @@ fi
 
 # TODO: implement test that detects whether the correct BLAS is actually used
 if [[ ${blas_impl:-} = "accelerate" ]]; then
-    LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_BLAS=OFF"
+    LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_BLAS=ON"
     LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_ACCELERATE=ON"
+    LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_BLAS_VENDOR=Apple"
 elif [[ ${blas_impl:-} = "mkl" ]]; then
     LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_BLAS=ON"
     LLAMA_ARGS="${LLAMA_ARGS} -DLLAMA_ACCELERATE=OFF"
