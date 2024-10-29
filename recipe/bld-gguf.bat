ae3a75761d6cc409a52f69845836bc5071319fc0
Jesse Noller <jnoller@gmail.com>
2024-10-29 13:24:11 -0600

    Remove multi line script invocation because of windows

---
+++ recipe/bld-gguf.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+cd gguf-py
+if errorlevel 1 exit 1
+
+:: Install the package using pip
+pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit 1
