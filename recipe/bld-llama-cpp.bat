ce8c535079347da879f7c1578bcb1e80da34a9be
Jesse Noller <jnoller@gmail.com>
2025-02-06 13:52:07 -0700

    Fix windows ctest invocation

--- recipe/bld-llama-cpp.bat
+++ recipe/bld-llama-cpp.bat
@@ -79,6 +79,6 @@ cmake --install build
 if errorlevel 1 exit 1
 
 pushd build
-ctest --output-on-failure build -j%CPU_COUNT% --test-dir build/tests
+ctest --output-on-failure -j%CPU_COUNT%
 if errorlevel 1 exit 1
 popd
