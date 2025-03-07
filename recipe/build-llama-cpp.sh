3e52ef011c8835affbc1db88bd6d866a248803c1
Jesse Noller <jnoller@gmail.com>
2025-03-07 12:10:53 -0700

    remove fma as it blows up linux

--- recipe/build-llama-cpp.sh
+++ recipe/build-llama-cpp.sh
@@ -44,29 +44,28 @@ fi
 
 # Configure CPU optimization flags based on the x86_64_opt variable
 
-if [[ $ARCH = "x86_64" ]]; then
-    if [[ ${x86_64_opt:-} = "v4" ]]; then
-        # Enable AVX-512 and all previous instruction sets (v4)
-        export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v4}"
-        export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v4}"
-        export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v4}"
-        LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON -DGGML_AVX2=ON -DGGML_AVX512=ON -DGGML_FMA=ON -DGGML_F16C=ON"
-        # TBD: -DGGML_AVX512_VBMI=ON -DGGML_AVX512_VNNI=ON -DGGML_AVX512_BF16=ON 
-        # May not want to enable these additional AVX512 instructions for all x86_64 builds
-    elif [[ ${x86_64_opt:-} = "v3" ]]; then
-        # Enable AVX, AVX2, FMA, F16C and all previous instruction sets (v3)
-        export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v3}"
-        export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v3}"
-        export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v3}"
-        LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON -DGGML_AVX2=ON"
-        # -DGGML_FMA=ON -DGGML_F16C=ON
-    elif [[ ${x86_64_opt:-} = "v2" ]]; then
-        # Enable AVX and all previous instruction sets (v2)
-        export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v2}"
-        export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v2}"
-        export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v2}"
-        LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON"
-    fi
+
+if [[ ${x86_64_opt:-} = "v4" ]]; then
+    # Enable AVX-512 and all previous instruction sets (v4)
+    export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v4}"
+    export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v4}"
+    export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v4}"
+    LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON -DGGML_AVX2=ON -DGGML_AVX512=ON -DGGML_FMA=ON -DGGML_F16C=ON"
+    # TBD: -DGGML_AVX512_VBMI=ON -DGGML_AVX512_VNNI=ON -DGGML_AVX512_BF16=ON 
+    # May not want to enable these additional AVX512 instructions for all x86_64 builds
+elif [[ ${x86_64_opt:-} = "v3" ]]; then
+    # Enable AVX, AVX2, FMA, F16C and all previous instruction sets (v3)
+    export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v3}"
+    export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v3}"
+    export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v3}"
+    LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON -DGGML_AVX2=ON"
+    # -DGGML_FMA=ON -DGGML_F16C=ON
+elif [[ ${x86_64_opt:-} = "v2" ]]; then
+    # Enable AVX and all previous instruction sets (v2)
+    export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v2}"
+    export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v2}"
+    export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v2}"
+    LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON"
 fi
 
 # Set defaults for instruction sets if not already set
