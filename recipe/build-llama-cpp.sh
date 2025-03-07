65b4756e318279ae72982f1dfe6d4ebf7eaffa7c
Jesse Noller <jnoller@gmail.com>
2025-03-07 12:33:21 -0700

    not sure why its compiling aarch64 code in the x86 upstream logic

--- recipe/build-llama-cpp.sh
+++ recipe/build-llama-cpp.sh
@@ -43,25 +43,21 @@ else
 fi
 
 # Configure CPU optimization flags based on the x86_64_opt variable
+# See: https://github.com/ggml-org/llama.cpp/blob/3ffbbd5ce130859be91909e9b77d4c1962a6be2c/ggml/CMakeLists.txt#L96
+# for the upstream makefile logic for the CPU optimization flags
 
 
 if [[ ${x86_64_opt:-} = "v4" ]]; then
-    # Enable AVX-512 and all previous instruction sets (v4)
     export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v4}"
     export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v4}"
     export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v4}"
     LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON -DGGML_AVX2=ON -DGGML_AVX512=ON"
-    # TBD: -DGGML_AVX512_VBMI=ON -DGGML_AVX512_VNNI=ON -DGGML_AVX512_BF16=ON 
-    # May not want to enable these additional AVX512 instructions for all x86_64 builds
 elif [[ ${x86_64_opt:-} = "v3" ]]; then
-    # Enable AVX, AVX2, FMA, F16C and all previous instruction sets (v3)
     export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v3}"
     export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v3}"
     export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v3}"
     LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AVX=ON -DGGML_AVX2=ON"
-    # -DGGML_FMA=ON -DGGML_F16C=ON
 elif [[ ${x86_64_opt:-} = "v2" ]]; then
-    # Enable AVX and all previous instruction sets (v2)
     export CXXFLAGS="${CXXFLAGS/march=nocona/march=x86-64-v2}"
     export CFLAGS="${CFLAGS/march=nocona/march=x86-64-v2}"
     export CPPFLAGS="${CPPFLAGS/march=nocona/march=x86-64-v2}"
@@ -105,6 +101,9 @@ fi
 if [[ ! "${LLAMA_ARGS}" =~ "-DGGML_AMX_BF16=" ]]; then
     LLAMA_ARGS="${LLAMA_ARGS} -DGGML_AMX_BF16=OFF"
 fi
+if [[ ! "${LLAMA_ARGS}" =~ "-DGGML_CUDA_F16=" ]]; then
+    LLAMA_ARGS="${LLAMA_ARGS} -DGGML_CUDA_F16=OFF"
+fi
 
 cmake -S . -B build \
     -G Ninja \
@@ -116,7 +115,9 @@ cmake -S . -B build \
     -DLLAMA_BUILD_TESTS=ON  \
     -DBUILD_SHARED_LIBS=ON  \
     -DGGML_NATIVE=OFF \
-    -DLLAMA_CURL=ON
+    -DLLAMA_CURL=ON \
+    -DGGML_BACKEND_DL=OFF \
+    -DGGML_CPU_AARCH64=OFF
 
 cmake --build build --config Release --verbose
 cmake --install build
