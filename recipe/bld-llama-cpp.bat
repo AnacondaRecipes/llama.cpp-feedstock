830ff33964c368c676ea53732271e73db9a6ea29
Charles Bousseau <cbousseau@anaconda.com>
2025-09-16 17:30:11 -0400

    split binaries on windows

--- recipe/bld-llama-cpp.bat
+++ recipe/bld-llama-cpp.bat
@@ -29,10 +29,25 @@ if "%blas_impl%"=="mkl" (
 REM LLAMA build options
 set LLAMA_ARGS=-DLLAMA_BUILD_NUMBER=%LLAMA_BUILD_NUMBER% -DLLAMA_BUILD_COMMIT=%LLAMA_BUILD_COMMIT%
 set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_CURL=ON
-set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_SERVER=ON
-set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TOOLS=ON
-set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TESTS=ON
-set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_EXAMPLES=OFF
+if "%PKG_NAME%" == "libllama" (
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_SERVER=OFF
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TOOLS=OFF
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TESTS=OFF
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_EXAMPLES=OFF
+) else if "%PKG_NAME%" == "llama.cpp" (
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_SERVER=ON
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TOOLS=ON
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TESTS=OFF
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_EXAMPLES=OFF
+) else if "%PKG_NAME%" == "llama.cpp-tests" (
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_SERVER=OFF
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TOOLS=OFF
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_TESTS=ON
+    set LLAMA_ARGS=!LLAMA_ARGS! -DLLAMA_BUILD_EXAMPLES=OFF
+) else (
+    echo "Invalid package name: %PKG_NAME%"
+    exit 1
+)
 REM TODO add LLAMA_LLGUIDANCE?
 REM TODO set LLAMA_USE_SYSTEM_GGML once ggml gets its own feedstock
 
@@ -53,8 +68,10 @@ if errorlevel 1 exit 1
 cmake --install build
 if errorlevel 1 exit 1
 
-pushd build
-REM test-tokenizers-ggml-vocabs requires git-lfs to download the model files
-ctest -L main -C Release --output-on-failure -j%CPU_COUNT% --timeout 900 -E "test-tokenizers-ggml-vocabs"
-if errorlevel 1 exit 1
-popd
+if "%PKG_NAME%" == "llama-cpp-tests" (
+    pushd build
+    REM test-tokenizers-ggml-vocabs requires git-lfs to download the model files
+    ctest -L main -C Release --output-on-failure -j%CPU_COUNT% --timeout 900 -E "test-tokenizers-ggml-vocabs"
+    if errorlevel 1 exit 1
+    popd
+)
